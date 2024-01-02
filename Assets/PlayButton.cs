39cbb69f165058f1da55500993c85b8973329666
powderduel <hhurhxhdyruw@gmail.com>
2024-01-03 03:47:09 +0800

    pack

--- Assets/PlayButton.cs
+++ Assets/PlayButton.cs
@@ -4,17 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class PlayButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     public void Play(){
         SceneManager.LoadScene("SampleScene");
     }
